8b60ddd57c528c45a6d596f2d4b034f6c69b4110
reboound <al@wolfsburg.de>
2024-10-20 15:52:31 +0200

    Initial commit

---
+++ src/de/vfh/theoinf/lang/Language1.java
@@ -0,0 +1,13 @@
+package de.vfh.theoinf.lang;
+
+public class Language1 {
+    /**
+     * accept words over {a,b,c} that have b after every a.
+     * @param word Eingabewort
+     * @return true, wenn es ein Wort über {a,b,c} ist
+     * und hinter jedem a direkt ein b folgt.
+     */
+    public static boolean accept(String word) {
+        return word.matches("(a?b|c)*");
+    }
+}
---
+++ test/de/vfh/theoinf/lang/Language1Test.java
@@ -0,0 +1,54 @@
+package de.vfh.theoinf.lang;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Language1Test {
+
+	@Test
+	public void testAccept() {
+		String[] tests = {
+				"",
+				"b",
+				"c",
+				"ab",
+				"abc",
+				"abb",
+				"abab",
+				"bab",
+				"cab",
+				"bc",
+				"bcb",
+				"bcababcbab",
+				"abcabcabcabcabcabc"
+		};
+		for (String s : tests) {
+			assertTrue("Wort <"+s+"> sollte akzeptiert werden!", Language1.accept(s));
+		}
+	}
+
+	@Test
+	public void testNotAccept() {
+		String[] tests = {
+				"a",
+				"aa",
+				"ac",
+				"ba",
+				"d",
+				"e",
+				"0",
+				"ca",
+				"aba",
+				"abcd",
+				"baa",
+				"bac",
+				"cba",
+				"abcabcabcabcabca"
+		};
+		for (String s : tests) {
+			assertFalse("Wort <"+s+"> sollte nicht akzeptiert werden!", Language1.accept(s));
+		}
+	}
+
+}
